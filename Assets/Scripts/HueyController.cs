e85eabcbae54c9bfff193d12942fb57e4ab3cbde
Brayan Soto <brayansoto413@gmail.com>
2024-02-06 20:39:49 -0500

    FixedHueyController
    Working Proto of game.

--- Assets/Scripts/HueyController.cs
+++ Assets/Scripts/HueyController.cs
@@ -7,16 +7,18 @@ public class HueyController : MonoBehaviour
     public float attackDamage = 10f;
     public LayerMask groundLayer;
     public int health = 3;
-    public float moveSpeed = 1.0f;
+    public float moveSpeed = 5.0f;
     public float jumpForce = 10.0f;
     public float immunityDuration = 2f;
     public float knockbackForce = 5f;
     private bool isGrounded;
     private bool isImmune = false;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -30,17 +32,15 @@ public class HueyController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded || Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             Debug.Log("Jumping");
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
-        Vector2 movement = new Vector2(horizontalInput, 0f);
-
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
 
         //Attack
         if (Input.GetKeyDown(KeyCode.S))
         {
-            
+            // Implement attack logic here
         }
     }
 
@@ -51,16 +51,16 @@ public class HueyController : MonoBehaviour
             TakeDamage();
 
             Vector2 knockbackDirection = (transform.position - collision.transform.position).normalized;
-            GetComponent<Rigidbody2D>().AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
+            rb.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
 
             StartImmunityCooldown();
         }
     }
 
-    void TakeDamage() 
+    void TakeDamage()
     {
-        health = health -1;
-        Debug.Log(health);
+        health--;
+        Debug.Log("Health: " + health);
     }
 
     void StartImmunityCooldown()
